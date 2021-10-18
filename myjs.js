5d1a8c97342928c7868022a29d8d14ed23559224
samira talanchi <samira73talanchi@gmail.com>
2021-10-18 17:55:02 +0330

    add change doted color animation

--- myjs.js
+++ myjs.js
@@ -1,3 +1,16 @@
 function displayTimeLine() {
-    document.getElementById("progress-bar").style.animation="timeline 12s linear";
+    document.getElementById("progress-bar").style.animation="timeline 12s forwards linear";
+    document.getElementById("jan").style.animation="changecolor 100ms linear forwards 1440ms";
+    document.getElementById("feb").style.animation="changecolor 100ms linear forwards 2400ms";
+    document.getElementById("mar").style.animation="changecolor 100ms linear forwards 3360ms";
+    document.getElementById("apr").style.animation="changecolor 100ms linear forwards 4320ms";
+    document.getElementById("may").style.animation="changecolor 100ms linear forwards 5280ms";
+    document.getElementById("jun").style.animation="changecolor 100ms linear forwards 6240ms";
+    document.getElementById("jul").style.animation="changecolor 100ms linear forwards 7200ms";
+    document.getElementById("aug").style.animation="changecolor 100ms linear forwards 8160ms";
+    document.getElementById("sep").style.animation="changecolor 100ms linear forwards 9120ms";
+    document.getElementById("oct").style.animation="changecolor 100ms linear forwards 10080ms";
+    document.getElementById("nov").style.animation="changecolor 100ms linear forwards 11040ms";
+    document.getElementById("dec").style.animation="changecolor 100ms linear forwards 12000ms";
+
 }
